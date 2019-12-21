62d8cdfd3105551bb78272fa50a9fb80ec2396e0
Jean Tampon <jean.tampon@gmail.com>
2019-12-21 16:06:33 +0100

    Add first dist gradient computation

--- include/grid_map.hpp
+++ include/grid_map.hpp
@@ -1,6 +1,30 @@
 #pragma once
 
 #include <vector>
+#include <list>
+
+
+struct Cell
+{
+	enum CellContent
+	{
+		Undefined,
+		Empty,
+		Filled,
+		Target
+	};
+
+	Cell(CellContent content_ = CellContent::Empty, sf::Vector2i position_ = sf::Vector2i(0, 0))
+		: content(content_)
+		, position(position_)
+		, dist_to_target(-1)
+	{}
+
+	CellContent content;
+	sf::Vector2i position;
+	int32_t dist_to_target;
+};
+
 
 struct GridMap
 {
@@ -9,24 +33,128 @@ struct GridMap
 		, width(width_)
 		, height(height_)
 	{
-
+		for (uint32_t y(0U); y < height; ++y) {
+			for (uint32_t x(0U); x < width; ++x) {
+				map.emplace_back(Cell::Empty, sf::Vector2i(x, y));
+			}
+		}
 	}
 
-	uint32_t getCellAtt(int32_t x, int32_t y)
+	void setCellAt(int32_t x, int32_t y, Cell::CellContent value)
 	{
-		// Edge cases
-		if (x < 0 || x >= width) {
-			return 0;
+		if (isValidCell(x, y)) {
+			map[y * width + x].content = value;
 		}
-		if (y < 0 || y >= height) {
-			return 0;
+	}
+
+	Cell::CellContent getContentAt(int32_t x, int32_t y) const
+	{
+		if (!isValidCell(x, y)) {
+			return Cell::Undefined;
+		}
+	
+		return map[y * width + x].content;
+	}
+
+	Cell* getCellAt(int32_t x, int32_t y)
+	{
+		if (!isValidCell(x, y)) {
+			return nullptr;
 		}
 
-		return map[y * width + x];
+		return &map[y * width + x];
+	}
+
+	const Cell* getCellAt(int32_t x, int32_t y) const
+	{
+		if (!isValidCell(x, y)) {
+			return nullptr;
+		}
+
+		return &map[y * width + x];
+	}
+
+	bool isValidCell(int32_t x, int32_t y) const
+	{
+		if (x < 0 || uint32_t(x) >= width) {
+			return false;
+		}
+		if (y < 0 || uint32_t(y) >= height) {
+			return false;
+		}
+		return true;
+	}
+
+	void compute_dist_to(uint32_t x, uint32_t y)
+	{
+		for (Cell& cell : map)
+		{
+			cell.dist_to_target = -1;
+		}
+
+		std::list<Cell*> to_visit;
+		Cell* start_cell = getCellAt(x, y);
+		if (start_cell)
+		{
+			start_cell->dist_to_target = 0;
+			to_visit.push_back(start_cell);
+			while (!to_visit.empty())
+			{
+				Cell* cell = to_visit.front();
+				const int32_t current_dist = cell->dist_to_target;
+				const sf::Vector2i& current_pos(cell->position);
+
+				// Top
+				Cell* top_cell = getCellAt(current_pos.x, current_pos.y - 1);
+				if (top_cell) {
+					if (top_cell->content != Cell::Filled) {
+						if (top_cell->dist_to_target == -1 || top_cell->dist_to_target > current_dist + 1) {
+							top_cell->dist_to_target = current_dist + 1;
+							to_visit.push_back(top_cell);
+						}
+					}
+				}
+
+				// Bot
+				Cell* bot_cell = getCellAt(current_pos.x, current_pos.y + 1);
+				if (bot_cell) {
+					if (bot_cell->content != Cell::Filled) {
+						if (bot_cell->dist_to_target == -1 || bot_cell->dist_to_target > current_dist + 1) {
+							bot_cell->dist_to_target = current_dist + 1;
+							to_visit.push_back(bot_cell);
+						}
+					}
+				}
+
+				// Left
+				Cell* left_cell = getCellAt(current_pos.x - 1, current_pos.y);
+				if (left_cell) {
+					if (left_cell->content != Cell::Filled) {
+						if (left_cell->dist_to_target == -1 || left_cell->dist_to_target > current_dist + 1) {
+							left_cell->dist_to_target = current_dist + 1;
+							to_visit.push_back(left_cell);
+						}
+					}
+				}
+
+				// Right
+				Cell* right_cell = getCellAt(current_pos.x + 1, current_pos.y);
+				if (right_cell) {
+					if (right_cell->content != Cell::Filled) {
+						if (right_cell->dist_to_target == -1 || right_cell->dist_to_target > current_dist + 1) {
+							right_cell->dist_to_target = current_dist + 1;
+							to_visit.push_back(right_cell);
+						}
+					}
+				}
+
+				to_visit.pop_front();
+			}
+		}
 	}
 
 	const uint32_t cell_size;
 	const uint32_t width;
 	const uint32_t height;
-	std::vector<uint32_t> map;
+	std::vector<Cell> map;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,38 @@
 #include <iostream>
-#include <SFML/Graphics.hpp>
+#include "graphic_utils.hpp"
 #include "grid_map.hpp"
 
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
+	const uint32_t WIN_WIDTH = 1000;
+	const uint32_t WIN_HEIGHT = 1000;
+	sf::RenderWindow window(sf::VideoMode(WIN_WIDTH, WIN_HEIGHT), "PathFinder");
+
+	GridMap map(50, 20, 20);
+	map.setCellAt(6, 5, Cell::Filled);
+	map.setCellAt(7, 5, Cell::Filled);
+	map.setCellAt(8, 5, Cell::Filled);
+	map.setCellAt(9, 5, Cell::Filled);
+	map.setCellAt(10, 5, Cell::Filled);
+	map.setCellAt(10, 6, Cell::Filled);
+	map.setCellAt(10, 7, Cell::Filled);
+	map.setCellAt(10, 8, Cell::Filled);
+	map.setCellAt(10, 9, Cell::Filled);
+	map.setCellAt(10, 10, Cell::Filled);
+	map.setCellAt(10, 11, Cell::Filled);
+	map.setCellAt(10, 12, Cell::Filled);
+	map.setCellAt(10, 13, Cell::Filled);
+	map.setCellAt(10, 14, Cell::Filled);
+	map.setCellAt(10, 15, Cell::Filled);
+	map.setCellAt(9, 15, Cell::Filled);
+	map.setCellAt(8, 15, Cell::Filled);
+	map.setCellAt(7, 15, Cell::Filled);
+	map.setCellAt(6, 15, Cell::Filled);
+
+	map.setCellAt(19, 10, Cell::Target);
+
+	map.compute_dist_to(19, 10);
 
 	while (window.isOpen())
 	{
@@ -18,6 +44,9 @@ int main()
 		}
 
 		window.clear();
+
+		draw_grid(map, window);
+
 		window.display();
 	}
 
